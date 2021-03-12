a2b7cbff63e801bf939843ed9dceb125ca1a4d77
JasonDormier <jmdormier@gmail.com>
2021-03-11 23:23:52 -0800

    lab complete

---
+++ flowers/pickup.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-west-2' });
+const sns = new AWS.SNS();
+const faker = require('faker')
+
+const topic = 'arn:aws:sns:us-west-2:409687769195:pickup';
+//const orderItem = process.env[2];
+
+const order = {
+  orderId: faker.random.uuid(),
+  customer: faker.name.findName(),
+  vendorId: 'https://sqs.us-west-2.amazonaws.com/409687769195/flowers',
+}
+
+const params = {
+  //MessageId: faker.random.uuid(),
+  TopicArn: topic,
+  Message: JSON.stringify(order),
+};
+
+setInterval(() => {
+
+  sns.publish(params).promise().then(console.log).catch(console.error);
+
+}, 5000)
+
